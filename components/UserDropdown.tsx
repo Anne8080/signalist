6557b76ce5d1a5d62c3dcdbc36f59a1932a21db9
Anne8080 <ireakin10@gmail.com>
2026-01-06 10:15:52 +0100

    made the avatar to have a cursor-pointer feature

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -27,7 +27,7 @@ const UserDropdown = () => {
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
-                <Button variant="ghost" className={'flex items-center gap 3 text-gray-4 hover:text-yellow-500'}>
+                <Button variant="ghost" className={'flex items-center gap 3 text-gray-4 hover:text-yellow-500 cursor-pointer'}>
                     <Avatar className={'h-8 w-8'}>
                         {/*<AvatarImage src="https://github.com/shadcn.png" alt="" />*/}
                         <AvatarFallback className={'bg-yellow-500 text-yellow-900 text-sm font-bold'}>
